1ad0b9ad4d59499aea5582cad54fcbf9044ecad7
Daniel Gregson <dpgregson@hotmail.com>
2021-12-03 22:40:29 +0000

    chore: day 3 part 1 solution
    author: [DG]

---
+++ src/dg.adventofcode.2021.tests/BinaryDiagnosticTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using dg.adventofcode._2021.crosscutting;
+using dg.adventofcode._2021.Days.Day3;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace dg.adventofcode._2021.tests
+{
+    public class BinaryDiagnosticTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public BinaryDiagnosticTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Theory]
+        [InlineData("00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010")]
+        public void CalcPowerConsumption(params string[] numberData)
+        {
+            var binaryDiag = new BinaryDiagnostic();
+
+            var result = binaryDiag.Calculate(numberData.ToList());
+
+            Assert.Equal(198, result);
+        }
+
+        [Fact]
+        public void ActualTest()
+        {
+            var binaryDiag = new BinaryDiagnostic();
+
+            var textFileLoader = new TextFileLoader();
+            var testData = textFileLoader.LoadStringListFromStrings("TestData\\BinaryDiagnosticData.txt");
+            var result = binaryDiag.Calculate(testData);
+
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ src/dg.adventofcode.2021/Days/Day3/BinaryDiagnostic.cs
@@ -0,0 +1,54 @@
+﻿namespace dg.adventofcode._2021.Days.Day3
+{
+    public class BinaryDiagnostic
+    {
+        public int Calculate(List<string> numberData)
+        {
+            var binarySize = numberData.First().Length;
+            var finalBits = "";
+
+            for (var i = 0; i < binarySize; i++)
+            {
+                var zeros = 0;
+                var ones = 0;
+
+                foreach (var t in numberData)
+                {
+                    if (t[i].ToString() == "1")
+                    {
+                        ones++;
+                    }
+                    else
+                    {
+                        zeros++;
+                    }
+                }
+
+                if (zeros > ones)
+                {
+                    finalBits += "0";
+                }
+                else
+                {
+                    finalBits += "1";
+                }
+            }
+
+            var gammaRate = Convert.ToInt32(finalBits, 2);
+            var epsilonRate = Convert.ToInt32(InvertBitString(finalBits), 2);
+
+            return gammaRate * epsilonRate;
+        }
+
+        private static string InvertBitString(string finalBits)
+        {
+            var newBits = "";
+            foreach (var finalBit in finalBits)
+            {
+                newBits += finalBit.ToString() == "1" ? "0" : "1";
+            }
+
+            return newBits;
+        }
+    }
+}
