6dfb834cc97a152fa1b1968ddc55f72d67e9e875
Daniel Gregson <dpgregson@hotmail.com>
2021-12-04 13:01:35 +0000

    chore: setup ready for day 4
    author: [DG]

---
+++ src/dg.adventofcode.2021/Days/Day4/GiantSquid.cs
@@ -0,0 +1,10 @@
+﻿namespace dg.adventofcode._2021.Days.Day4
+{
+    public class GiantSquid
+    {
+        public int GetWinningBoardScore(List<string> gameInput)
+        {
+            return 0;
+        }
+    }
+}
