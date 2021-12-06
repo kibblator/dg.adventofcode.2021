f5438b2162c28fb4eb46785c1edf683ab95cbcce
Daniel Gregson <kornfreakuk@hotmail.com>
2021-12-06 22:32:07 +0000

    chore: refactor solution to be ALOT more performant
    author: [DG]

--- src/dg.adventofcode.2021/Days/Day6/Lanternfish.cs
+++ src/dg.adventofcode.2021/Days/Day6/Lanternfish.cs
@@ -6,88 +6,73 @@
         private const int DaysUntilFishBreed = 6;
         private const int FishBirthingAge = 0;
 
-        public int GetNumLanternfish(string lanternFishState, int daysToGet, Action<string> logOutput = null)
+        public long GetNumLanternfish(string lanternFishState, int daysToGet, Action<string> logOutput = null)
         {
             var fishState = lanternFishState.Split(',').Select(int.Parse).OrderBy(f => f);
 
-            //var numFish = ProcessFish(fishState, daysToGet, logOutput);
             var numFish = GetFishBetter(fishState, daysToGet);
             return numFish;
         }
 
-        private static int ProcessFish(IEnumerable<int> fishState, int daysRemaining, Action<string> logOutput)
-        {
-            var totalDays = daysRemaining;
-
-            while (true)
-            {
-                if (daysRemaining == 0) return fishState.Count();
-
-                var newToAdd = 0;
-                var newFishState = new List<int>();
-
-                foreach (var fish in fishState)
-                {
-                    var newFish = fish - 1;
-                    if (newFish < 0)
-                    {
-                        newToAdd++;
-                        newFish = 6;
-                    }
-
-                    newFishState.Add(newFish);
-                }
-
-                while (newToAdd > 0)
-                {
-                    newFishState.Add(8);
-                    newToAdd--;
-                }
-
-                fishState = newFishState;
-                daysRemaining -= 1;
-
-                if (logOutput != null)
-                {
-                    logOutput(
-                        $"Fish Day {totalDays - daysRemaining}: {string.Join(",", newFishState.Select(f => f.ToString()))}");
-                }
-            }
-        }
-
-        private static int GetFishBetter(IEnumerable<int> initialFishes, int days)
+        private static long GetFishBetter(IEnumerable<int> initialFishes, int days)
         {
             var fishAgeList = SetUpBlankFishCounts();
 
-            foreach (var fish in initialFishes)
-            {
-                // Tally how many fish exist at each current age
-                fishAgeList[fish]++;
-            }
+            TallyCurrentFishAges(initialFishes, fishAgeList);
 
             while (FishAreLiving(days))
             {
-                var newFishToBeAdded = fishAgeList[FishBirthingAge];
+                var newFishToBeAdded = GetFishAboutToPop(fishAgeList);
+                AgeCurrentFish(fishAgeList);
 
-                for (var fishAge = 0; fishAge < DaysUntilFishBreedNewBorn; fishAge++)
-                {
-                    fishAgeList[fishAge] = fishAgeList[fishAge + 1];
-                }
-
-                if (newFishToBeAdded > 0)
-                {
-                    fishAgeList[DaysUntilFishBreed] += newFishToBeAdded;
-                    fishAgeList[DaysUntilFishBreedNewBorn] = newFishToBeAdded;
-                }
-                else
-                {
-                    fishAgeList[DaysUntilFishBreedNewBorn] = 0;
-                }
+                GetFishPregnantAgain(fishAgeList, newFishToBeAdded);
+                GiveBirthToBabyFish(fishAgeList, newFishToBeAdded);
 
                 days--;
             }
 
+            return GetCurrentFishCount(fishAgeList);
+        }
+
+        private static void AgeCurrentFish(IDictionary<int, long> fishAgeList)
+        {
+            for (var fishAge = 0; fishAge < DaysUntilFishBreedNewBorn; fishAge++)
+            {
+                fishAgeList[fishAge] = fishAgeList[fishAge + 1];
+            }
+        }
+
+        private static void TallyCurrentFishAges(IEnumerable<int> initialFishes, IDictionary<int, long> fishAgeList)
+        {
+            foreach (var fish in initialFishes)
+            {
+                fishAgeList[fish]++;
+            }
+        }
+
+        private static long GetCurrentFishCount(Dictionary<int, long> fishAgeList)
+        {
             return fishAgeList.Sum(fishes => fishes.Value);
+        }
+
+        private static void GiveBirthToBabyFish(IDictionary<int, long> fishAgeList, long newFishToBeAdded)
+        {
+            fishAgeList[DaysUntilFishBreedNewBorn] = newFishToBeAdded;
+        }
+
+        private static void GetFishPregnantAgain(IDictionary<int, long> fishAgeList, long newFishToBeAdded)
+        {
+            fishAgeList[DaysUntilFishBreed] += newFishToBeAdded;
+        }
+
+        private static void AgeCurrentFish(IDictionary<int, long> fishAgeList, int fishAge)
+        {
+            fishAgeList[fishAge] = fishAgeList[fishAge + 1];
+        }
+
+        private static long GetFishAboutToPop(IReadOnlyDictionary<int, long> fishAgeList)
+        {
+            return fishAgeList[FishBirthingAge];
         }
 
         private static bool FishAreLiving(int days)
@@ -95,9 +80,9 @@
             return days > 0;
         }
 
-        private static Dictionary<int, int> SetUpBlankFishCounts()
+        private static Dictionary<int, long> SetUpBlankFishCounts()
         {
-            return Enumerable.Range(0, DaysUntilFishBreedNewBorn + 1).ToDictionary(k => k, _ => 0);
+            return Enumerable.Range(0, DaysUntilFishBreedNewBorn + 1).ToDictionary(k => k, _ => (long)0);
         }
     }
 }
