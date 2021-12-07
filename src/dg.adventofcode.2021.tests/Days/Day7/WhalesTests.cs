6c9ef2b7dd5857269c6d9eefa5cb0d34d5947bef
Daniel Gregson <kornfreakuk@hotmail.com>
2021-12-07 21:12:55 +0000

    chore: Day 7 Part 1 complete
    author: [DG]

---
+++ src/dg.adventofcode.2021.tests/Days/Day7/WhalesTests.cs
@@ -0,0 +1,59 @@
+﻿using dg.adventofcode._2021.crosscutting;
+using dg.adventofcode._2021.Days.Day7;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace dg.adventofcode._2021.tests.Days.Day7
+{
+    public class WhalesTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public WhalesTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Theory]
+        [InlineData("16,1,2,0,4,2,7,1,2,14")]
+        public void ExampleTestCase(string input)
+        {
+            var fuelSpent = new Whales().CalculateFuel(input);
+
+            Assert.Equal(37, fuelSpent);
+        }
+
+        [Fact]
+        public void Part1Test()
+        {
+            var textFileLoader = new TextFileLoader();
+            var input = textFileLoader.LoadString("TestData\\Whales.txt");
+
+            var fuelSpent = new Whales();
+            var result = fuelSpent.CalculateFuel(input);
+
+            _output.WriteLine(result.ToString());
+        }
+
+        [Theory]
+        [InlineData("3,4,3,1,2")]
+        public void ExampleTestCase2(string input)
+        {
+            var fuelCost = new Whales().CalculateFuel(input, true);
+
+            Assert.Equal(168, fuelCost);
+        }
+
+        [Fact]
+        public void Part2Test()
+        {
+            var textFileLoader = new TextFileLoader();
+            var input = textFileLoader.LoadString("TestData\\Whales.txt");
+
+            var fuelCost = new Whales();
+            var result = fuelCost.CalculateFuel(input, true);
+
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ src/dg.adventofcode.2021/Days/Day7/Whales.cs
@@ -0,0 +1,37 @@
+﻿namespace dg.adventofcode._2021.Days.Day7
+{
+    public class Whales
+    {
+        public int CalculateFuel(string input, bool part2 = false)
+        {
+            var positions = input.Split(',').Select(int.Parse).ToList();
+
+            var maxPosition = positions.Max();
+            var minPosition = positions.Min();
+            var fuelCost = CalcFuelCost(positions, minPosition, maxPosition);
+
+            return fuelCost;
+        }
+
+        private int CalcFuelCost(List<int> positions, int minPosition, int maxPosition)
+        {
+            var fuelCost = int.MaxValue;
+            while (maxPosition >= minPosition)
+            {
+                var cost = WorkOutCost(positions, maxPosition);
+                if (cost < fuelCost)
+                    fuelCost = cost;
+                maxPosition--;
+            }
+
+            return fuelCost;
+        }
+
+        private int WorkOutCost(List<int>positions, int targetPosition)
+        {
+            return positions.Select(p => p > targetPosition
+                ? p - targetPosition
+                : targetPosition - p).Sum();
+        }
+    }
+}
