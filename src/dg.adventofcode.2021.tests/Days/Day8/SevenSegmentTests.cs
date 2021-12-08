f69cf24700673c8a127ec82716c85e9a81261a23
Daniel Gregson <kornfreakuk@hotmail.com>
2021-12-08 08:58:25 +0000

    chore: Day 8 Part 1 Complete
    author: [DG]

---
+++ src/dg.adventofcode.2021.tests/Days/Day8/SevenSegmentTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using dg.adventofcode._2021.crosscutting;
+using dg.adventofcode._2021.Days.Day8;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace dg.adventofcode._2021.tests.Days.Day8
+{
+    public class SevenSegmentTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public SevenSegmentTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        public static IEnumerable<object> Data =>
+            new object[]
+            {
+                new string[]
+                {
+                    "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+                    "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+                    "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+                    "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+                    "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+                    "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+                    "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+                    "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+                    "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+                    "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
+                }
+            };
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void ExampleTestCase(params string[] input)
+        {
+            var occurrences = new SevenSegment().CalcOccurrences(input.ToList());
+
+            Assert.Equal(26, occurrences);
+        }
+
+        [Fact]
+        public void Part1Test()
+        {
+            var textFileLoader = new TextFileLoader();
+            var input = textFileLoader.LoadStringListFromStrings("TestData\\SevenSegment.txt");
+
+            var occurrences = new SevenSegment();
+            var result = occurrences.CalcOccurrences(input);
+
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ src/dg.adventofcode.2021/Days/Day8/SevenSegment.cs
@@ -0,0 +1,36 @@
+﻿namespace dg.adventofcode._2021.Days.Day8
+{
+    public class SevenSegment
+    {
+        public int CalcOccurrences(List<string> input)
+        {
+            var numberOccurrences = Enumerable.Range(0, 10).ToDictionary(k => k, _ => 0);
+
+            var outputList = input
+                .Select(line => line.Split(" | ")[1])
+                .Select(outputValuesSection => outputValuesSection.Split(' ').ToList())
+                .SelectMany(outputValues => outputValues);
+
+            foreach (var outputValue in outputList)
+            {
+                switch (outputValue.Length)
+                {
+                    case 2:
+                        numberOccurrences[1]++;
+                        break;
+                    case 3:
+                        numberOccurrences[7]++;
+                        break;
+                    case 4:
+                        numberOccurrences[4]++;
+                        break;
+                    case 7:
+                        numberOccurrences[8]++;
+                        break;
+                }
+            }
+
+            return numberOccurrences.Sum(o => o.Value);
+        }
+    }
+}
