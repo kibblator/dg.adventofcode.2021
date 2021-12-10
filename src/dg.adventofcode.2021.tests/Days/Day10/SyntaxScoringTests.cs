35bf4ada9d7b7a06769c13b2a5d7298e1e1f5abc
Daniel Gregson <kornfreakuk@hotmail.com>
2021-12-10 21:53:01 +0000

    chore: Completed day 10 parts 1 and 2
    author: [DG]

---
+++ src/dg.adventofcode.2021.tests/Days/Day10/SyntaxScoringTests.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using dg.adventofcode._2021.crosscutting;
+using dg.adventofcode._2021.Days.Day10;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace dg.adventofcode._2021.tests.Days.Day10
+{
+    public class SyntaxScoringTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public SyntaxScoringTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Theory]
+        [InlineData("()")]
+        [InlineData("([])")]
+        [InlineData("{()()()}")]
+        [InlineData("<([{}])>")]
+        [InlineData("[<>({}){}[([])<>]]")]
+        [InlineData("(((((((((())))))))))")]
+        public void ValidChunks_ReturnNoMismatched(string input)
+        {
+            var mismatched = new SyntaxScoring().ValidateInput(input);
+            Assert.Equal(string.Empty, mismatched);
+        }
+
+        public static IEnumerable<object> Data =>
+            new object[]
+            {
+                new string[]
+                {
+                    "[({(<(())[]>[[{[]{<()<>>",
+                    "[(()[<>])]({[<{<<[]>>(",
+                    "{([(<{}[<>[]}>{[]{[(<()>",
+                    "(((({<>}<{<{<>}{[]{[]{}",
+                    "[[<[([]))<([[{}[[()]]]",
+                    "[{[{({}]{}}([{[{{{}}([]",
+                    "{<[[]]>}<{[{[{[]{()[[[]",
+                    "[<(<(<(<{}))><([]([]()",
+                    "<{([([[(<>()){}]>(<<{{",
+                    "<{([{{}}[<[[[<>{}]]]>[]]"
+                }
+            };
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void ExampleTestCase(params string[] input)
+        {
+            var errorScore = new SyntaxScoring().GetErrorScore(input.ToList());
+
+            Assert.Equal(15, errorScore);
+        }
+
+        [Fact]
+        public void Part1Test()
+        {
+            var textFileLoader = new TextFileLoader();
+            var input = textFileLoader.LoadStringListFromStrings("TestData\\SyntaxScoring.txt");
+
+            var scoring = new SyntaxScoring();
+            var result = scoring.GetErrorScore(input);
+
+            _output.WriteLine(result.ToString());
+        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void ExampleTestCase_Part2(params string[] input)
+        {
+            var errorScore = new SyntaxScoring().GetAutoCompleteScore(input.ToList());
+
+            Assert.Equal(288957, errorScore);
+        }
+
+        [Fact]
+        public void Part2Test()
+        {
+            var textFileLoader = new TextFileLoader();
+            var input = textFileLoader.LoadStringListFromStrings("TestData\\SyntaxScoring.txt");
+
+            var scoring = new SyntaxScoring();
+            var result = scoring.GetAutoCompleteScore(input);
+
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ src/dg.adventofcode.2021/Days/Day10/SyntaxScoring.cs
@@ -0,0 +1,143 @@
+﻿using System.Globalization;
+using System.Linq.Expressions;
+
+namespace dg.adventofcode._2021.Days.Day10
+{
+    public class SyntaxScoring
+    {
+        private readonly List<string> _openingCharacters = new List<string>
+        {
+            "(",
+            "[",
+            "{",
+            "<",
+        };
+
+        private List<string> _closingCharacter = new List<string>
+        {
+             ")",
+             "]",
+             "}",
+             ">",
+        };
+
+        private readonly Dictionary<string, string> _charPairs = new Dictionary<string, string>
+        {
+            {"(",")"} ,
+            {"[","]"},
+            {"{","}"},
+            {"<",">"}
+        };
+
+        private readonly Dictionary<string, int> _mismatchScores = new Dictionary<string, int>()
+        {
+            { ")", 3 },
+            { "]", 57 },
+            { "}", 1197 },
+            { ">", 25137 }
+        };
+
+        private readonly Dictionary<string, int> _completionScores = new Dictionary<string, int>()
+        {
+            { ")", 1 },
+            { "]", 2 },
+            { "}", 3 },
+            { ">", 4 }
+        };
+
+        public int GetErrorScore(List<string> input)
+        {
+            var mismatchedList = new List<string>();
+
+            foreach (var line in input)
+            {
+                var mismatched = ValidateInput(line);
+                if (string.IsNullOrEmpty(mismatched) == false)
+                    mismatchedList.Add(mismatched);
+            }
+
+            return mismatchedList.Sum(mm => _mismatchScores[mm]);
+        }
+
+        public long GetAutoCompleteScore(List<string> input)
+        {
+            var incompleteLines = new List<string>();
+
+            foreach (var line in input)
+            {
+                var mismatched = ValidateInput(line);
+                if (string.IsNullOrEmpty(mismatched))
+                    incompleteLines.Add(line);
+            }
+
+            var scores = new List<long>();
+            foreach (var line in incompleteLines)
+            {
+                var completedInput = CompleteInput(line);
+                long score = completedInput.Aggregate<char, long>(0, (current, character) => current * 5 + _completionScores[character.ToString()]);
+                scores.Add(score);
+            }
+
+            scores = scores.OrderBy(s => s).ToList();
+
+            var index = scores.Count / 2;
+            return scores[index];
+        }
+
+        public string ValidateInput(string input)
+        {
+            var charStack = new Stack<string>();
+            var mismatched = "";
+
+            var characters = input.Select(i => i.ToString());
+            foreach (var character in characters)
+            {
+                if (IsOpening(character))
+                {
+                    charStack.Push(character);
+                }
+                else
+                {
+                    var mostRecentOpening = charStack.Pop();
+                    if (character != _charPairs[mostRecentOpening])
+                    {
+                        mismatched = character;
+                        return mismatched;
+                    }
+                }
+            }
+            return mismatched;
+        }
+
+        public string CompleteInput(string input)
+        {
+            var charStack = new Stack<string>();
+            var completionString = "";
+
+            var characters = input.Select(i => i.ToString());
+            foreach (var character in characters)
+            {
+                if (IsOpening(character))
+                {
+                    charStack.Push(character);
+                }
+                else
+                { 
+                    charStack.Pop();
+                }
+            }
+
+            while (charStack.Count > 0)
+            {
+                completionString += _charPairs[charStack.Pop()];
+            }
+
+            return completionString;
+        }
+
+        private bool IsOpening(string character)
+        {
+            return _openingCharacters.Contains(character);
+        }
+    }
+}
