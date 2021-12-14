c422bb697d64ed730c17abedad98239a362e7cb0
Daniel Gregson <kornfreakuk@hotmail.com>
2021-12-14 09:53:20 +0000

    chore: setup for Day 14
    author: [DG]

---
+++ src/dg.adventofcode.2021.tests/Days/Day14/ExtendedPolyTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using dg.adventofcode._2021.crosscutting;
+using dg.adventofcode._2021.Days.Day13;
+using dg.adventofcode._2021.Days.Day14;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace dg.adventofcode._2021.tests.Days.Day14
+{
+    public class ExtendedPolyTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public ExtendedPolyTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        public static IEnumerable<object> Data =>
+            new object[]
+            {
+                new string[]
+                {
+                    "NNCB",
+                    "",
+                    "CH -> B",
+                    "HH -> N",
+                    "CB -> H",
+                    "NH -> C",
+                    "HB -> C",
+                    "HC -> B",
+                    "HN -> C",
+                    "NN -> C",
+                    "BH -> H",
+                    "NC -> B",
+                    "NB -> B",
+                    "BN -> B",
+                    "BB -> N",
+                    "BC -> B",
+                    "CC -> N",
+                    "CN -> C"
+                }
+            };
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void ExampleTestCase(params string[] input)
+        {
+            var quantityDiff = new ExtendedPoly().GetQuantityDiff(input.ToList());
+
+            Assert.Equal(1588, quantityDiff);
+        }
+
+        [Fact]
+        public void Part1Test()
+        {
+            var textFileLoader = new TextFileLoader();
+            var input = textFileLoader.LoadStringListFromStrings("TestData\\ExtendedPoly.txt");
+
+            var extendedPoly = new ExtendedPoly();
+            var result = extendedPoly.GetQuantityDiff(input);
+
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ src/dg.adventofcode.2021/Days/Day14/ExtendedPoly.cs
@@ -0,0 +1,10 @@
+﻿namespace dg.adventofcode._2021.Days.Day14
+{
+    public class ExtendedPoly
+    {
+        public int GetQuantityDiff(List<string> input)
+        {
+            return 0;
+        }
+    }
+}
