c2288e002aed9beaee6aaefbbff098c7f4732343
Daniel Gregson <kornfreakuk@hotmail.com>
2021-12-16 20:25:32 +0000

    chore: Completed Day 16 Part 1
    author: [DG]

--- src/dg.adventofcode.2021.tests/Days/Day16/PacketDecoderTests.cs
+++ src/dg.adventofcode.2021.tests/Days/Day16/PacketDecoderTests.cs
@@ -1,5 +1,5 @@
-﻿using System.Linq;
-using dg.adventofcode._2021.crosscutting;
+﻿using dg.adventofcode._2021.crosscutting;
+using dg.adventofcode._2021.Days.Day16;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -15,22 +15,37 @@ namespace dg.adventofcode._2021.tests.Days.Day16
         }
 
         [Theory]
+        [InlineData("101111111000101000", 2021)]
+        [InlineData("00001", 1)]
+        [InlineData("00010", 2)]
+        [InlineData("00011", 3)]
+        [InlineData("01010", 10)]
+        [InlineData("10001001000000000", 20)]
+        public void LiteralPacketDecode(string binaryString, int expectedValue)
+        {
+            PacketDecoder.ParseLiteralValuePackage(binaryString, out var literalValue);
+            Assert.Equal(expectedValue, literalValue);
+        }
+
+        [Theory]
         [InlineData("8A004A801A8002F478", 16)]
         [InlineData("620080001611562C8802118E34", 12)]
+        [InlineData("38006F45291200", 9)]
         [InlineData("C0015000016115A2E0802F182340", 23)]
         [InlineData("A0016C880162017C3686B18A3D4780", 31)]
-        public void ExampleTestCase(string input, int versionSum)
+        public void OperatorPacketDecode(string input, int versionSum)
         {
-            var versionNumbers = new PacketDecoder().GetVersionSum(input.ToList());
+            var packetDecoder = new PacketDecoder();
+            var versionNumbers = packetDecoder.GetVersionSum(input);
 
-            Assert.Equal(40, versionNumbers);
+            Assert.Equal(versionSum, versionNumbers);
         }
 
         [Fact]
         public void Part1Test()
         {
             var textFileLoader = new TextFileLoader();
-            var input = textFileLoader.LoadStringListFromStrings("TestData\\PacketDecoder.txt");
+            var input = textFileLoader.LoadString("TestData\\PacketDecoder.txt");
 
             var packetDecoder = new PacketDecoder();
             var result = packetDecoder.GetVersionSum(input);
--- src/dg.adventofcode.2021/Days/Day16/PacketDecoder.cs
+++ src/dg.adventofcode.2021/Days/Day16/PacketDecoder.cs
@@ -2,9 +2,124 @@
 {
     public class PacketDecoder
     {
-        public int GetVersionSum(string input)
+        private Dictionary<string, string> _hexBinaryLookup = new Dictionary<string, string>
         {
-            return 0;
+            { "0", "0000" },
+            { "1", "0001" },
+            { "2", "0010" },
+            { "3", "0011" },
+            { "4", "0100" },
+            { "5", "0101" },
+            { "6", "0110" },
+            { "7", "0111" },
+            { "8", "1000" },
+            { "9", "1001" },
+            { "A", "1010" },
+            { "B", "1011" },
+            { "C", "1100" },
+            { "D", "1101" },
+            { "E", "1110" },
+            { "F", "1111" }
+        };
+
+        public long GetVersionSum(string input)
+        {
+            long versionSum = 0;
+            var decodedPacket = GetDecodedPacket(input, ref versionSum);
+            return versionSum;
+        }
+
+        public string GetDecodedPacket(string encodedInput, ref long versionSum)
+        {
+            var binaryString = GetBinaryStringFromHex(encodedInput);
+
+            while (binaryString.Any(b => b != '0'))
+            {
+                var packetVersion = BinaryToInt(binaryString, 0, 3);
+                var packetTypeId = BinaryToInt(binaryString, 3, 3);
+                binaryString = binaryString.Substring(6, binaryString.Length - 6);
+
+                versionSum += packetVersion;
+
+                switch (packetTypeId)
+                {
+                    case 4:
+                        binaryString = ParseLiteralValuePackage(binaryString, out var literalValue);
+                        break;
+                    default:
+                        binaryString = ParseOperatorPackage(binaryString);
+                        break;
+                }
+            }
+
+            return "";
+        }
+
+        private static long BinaryToInt(string binaryString, int startIndex, int length)
+        {
+            return Convert.ToInt64(binaryString.Substring(startIndex, length).PadLeft(4, '0'), 2);
+        }
+
+        private string ParseOperatorPackage(string binaryString)
+        {
+            var length = binaryString.Substring(0, 1) == "0" ? 15 : 11;
+            var lengthBits = binaryString.Substring(1, length);
+            var lengthOfSubPackets = BinaryToInt(lengthBits, 0, lengthBits.Length);
+
+            return binaryString.Substring(length + 1, binaryString.Length - (length + 1));
+        }
+
+        public static string ParseLiteralValuePackage(string binaryString, out long literalValue)
+        {
+            const int bitLength = 1;
+            const int chunkSize = 4;
+
+            //binaryString = RemoveZeroPadding(binaryString);
+
+            var lastBit = false;
+            var totalString = "";
+
+            while (lastBit == false)
+            {
+                var bit = ManagedSubstring(binaryString, 0, bitLength);
+                lastBit = bit == "0";
+                var numPart = ManagedSubstring(binaryString, bitLength, chunkSize);
+                totalString += numPart;
+                binaryString = binaryString.Substring(chunkSize + bitLength, binaryString.Length - (bitLength + chunkSize));
+            }
+
+            literalValue = BinaryToInt(totalString, 0, totalString.Length);
+
+            return binaryString;
+        }
+
+        private static string RemoveZeroPadding(string binaryString)
+        {
+            while (binaryString.Length % 4 != 0)
+            {
+                binaryString = binaryString.Substring(1, binaryString.Length - 1);
+            }
+
+            return binaryString;
+        }
+
+        private static string ManagedSubstring(string binaryString, int startIndex, int length)
+        {
+            try
+            {
+                binaryString = binaryString.Substring(startIndex, length);
+            }
+            catch (Exception)
+            {
+                binaryString = binaryString.PadLeft(length, '0');
+            }
+
+            return binaryString;
+        }
+
+        private string GetBinaryStringFromHex(string hex)
+        {
+            return string.Concat(hex.Select(h => _hexBinaryLookup[h.ToString()]));
         }
     }
 }
