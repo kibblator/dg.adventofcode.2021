0d397bb0bb054f4fac721fc32e655b59e0447fad
Daniel Gregson <kornfreakuk@hotmail.com>
2021-12-16 08:04:19 +0000

    chore: setup Day 16
    author: [DG]

---
+++ src/dg.adventofcode.2021.tests/Days/Day16/PacketDecoderTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using dg.adventofcode._2021.crosscutting;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace dg.adventofcode._2021.tests.Days.Day16
+{
+    public class PacketDecoderTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public PacketDecoderTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Theory]
+        [InlineData("8A004A801A8002F478", 16)]
+        [InlineData("620080001611562C8802118E34", 12)]
+        [InlineData("C0015000016115A2E0802F182340", 23)]
+        [InlineData("A0016C880162017C3686B18A3D4780", 31)]
+        public void ExampleTestCase(string input, int versionSum)
+        {
+            var versionNumbers = new PacketDecoder().GetVersionSum(input.ToList());
+
+            Assert.Equal(40, versionNumbers);
+        }
+
+        [Fact]
+        public void Part1Test()
+        {
+            var textFileLoader = new TextFileLoader();
+            var input = textFileLoader.LoadStringListFromStrings("TestData\\PacketDecoder.txt");
+
+            var packetDecoder = new PacketDecoder();
+            var result = packetDecoder.GetVersionSum(input);
+
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
--- src/dg.adventofcode.2021/Days/Day15/Chiton.cs
+++ src/dg.adventofcode.2021/Days/Day15/Chiton.cs
@@ -4,7 +4,40 @@
     {
         public int GetPathRisk(List<string> input)
         {
+            var width = input.First().Length;
+            var height = input.Count;
+            var graph = GetGraph(input, width, height);
+
+            //var numberOfNodes;
             
+            return 0;
+        }
+
+        private int[][] GetGraph(List<string> input, int width, int height)
+        {
+            var graph = new int[height][];
+
+            for(var y = 0; y < height; y++)
+            {
+                var line = input[y];
+                for (var x = 0; x < width; x++)
+                {
+                    graph[y][x] = int.Parse(line[x].ToString());
+                }
+            }
+
+            return graph;
+        }
+
+        public class Node
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+
+            public override string ToString()
+            {
+                return $"{x}{y}";
+            }
         }
     }
 }
